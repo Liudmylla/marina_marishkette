81a2702557fa61c055951fa2ddbcf2f6e8155929
Liudmylla <liudmyla12@yahoo.fr>
2020-12-03 23:16:25 +0100

    change header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,16 @@
 import React from 'react'
 
-<<<<<<< HEAD
-import logo from '../assets/images/banner.jpg';
-=======
-import logo from '../assets/images/lessia.png';
->>>>>>> 689cd5b279af960ea899b2401860770af90852e5
+import banner from '../assets/images/banner.jpg';
 import instagram from '../assets/images/insta.png';
 
 const Header = (props) => (
     <header id="header" className="alt">
-<<<<<<< HEAD
-        <span className="logo"><img src={logo} alt="" /></span>
+        <span className="logo"><img src={banner} alt="" /></span>
         <h1>𝕃'𝔸ℝ𝕋 𝔻𝕌 𝕄𝔸ℚ𝕌𝕀𝕃𝕃𝔸𝔾𝔼 ℙ𝔼ℝ𝕄𝔸ℕ𝔼ℕ𝕋 𝕄𝕆𝔻𝔼ℝℕ𝔼</h1>
          <p>06-40-57-99-17</p>
         <p>69 avenue de l'Adour 64600 Anglet</p>
      
     <a href="https://www.instagram.com/esthetique_beaute_64/"><span className="instagram"><img src={instagram} alt="" /></span></a>
-=======
-        {/* <span className="logo"><img src={logo} alt="" /></span>  */}
-        <h1>𝕃'𝔸ℝ𝕋 𝔻𝕌 𝕄𝔸ℚ𝕌𝕀𝕃𝕃𝔸𝔾𝔼 ℙ𝔼ℝ𝕄𝔸ℕ𝔼ℕ𝕋 𝕄𝕆𝔻𝔼ℝℕ𝔼</h1>
-        <p>Lessia : 06-40-57-99-17</p>
-        <p>69 avenue de l'Adour 64600 Anglet</p>
-     
-    {/* <a href="https://www.instagram.com/esthetique_beaute_64/"><span className="instagram"><img src={instagram} alt="" /></span></a> */}
->>>>>>> 689cd5b279af960ea899b2401860770af90852e5
         
     </header>
 )
